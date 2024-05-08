ee6f00d296512c6bd491c04093b2a631ccaaa5a9
Kugesh Rajasekaran <kumaran.kugesh@gmail.com>
2024-05-08 22:25:40 +0530

    updated angular cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { MatLegacyFormFieldModule as MatFormFieldModule } from '@angular/material/legacy-form-field';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatLegacyInputModule as MatInputModule } from '@angular/material/legacy-input';
 import { TreeChartComponent } from './components/tree/tree.component';
 import { ClipboardModule } from '@angular/cdk/clipboard';
 
@@ -16,9 +14,8 @@ import { ClipboardModule } from '@angular/cdk/clipboard';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    MatFormFieldModule,
     BrowserAnimationsModule,
-    MatInputModule,
+
     ClipboardModule,
   ],
   providers: [ClipboardModule],
